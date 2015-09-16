6d23a7b74c846ad31ee49263257d24f26c6a1822
Karla Garcia <karlamaria@gmail.com>
2015-09-16 17:23:41 -0300

    Adicionando configs para heroku: root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'people/index2', to: 'people#index2'
   resources :people
+  root 'people#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
