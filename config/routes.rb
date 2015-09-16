805a793eebe7da40ccda695cc78fbe35939d0ceb
Karla Garcia <karlamaria@gmail.com>
2015-09-16 16:20:42 -0300

    Adicionando opcao de integrar pessoas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'people/index2', to: 'people#index2'
   resources :people
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
